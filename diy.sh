cc5008c6f645d7458df146bf56b708aba019a323
dlonzyz <dlonzyz@gmail.com>
2024-09-08 12:40:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,3 +2,5 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
